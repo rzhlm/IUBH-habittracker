46ba8e068a682fb572f8986abf2f1b4da01b7158
rzhlm-MBA15 <118811618+rzhlm@users.noreply.github.com>
2024-10-20 17:27:39 +0200

    +subclasses for StorageStrategy

--- src/storage.py
+++ src/storage.py
@@ -1,1 +1,16 @@
-#placeholder for storage classes
+class StorageStrategy:
+    def add_habit(self, habit):
+        pass
+    def load_all(self):
+        pass
+    def save_all(self):
+        pass
+
+class JSONStorage(StorageStrategy):
+    pass
+    
+class PickleStorage(StorageStrategy):
+    pass
+    
+class SQLiteStorage(StorageStrategy):
+    pass
