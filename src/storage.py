d32c583851d3fdee45994ff0a182e7476fcfa710
rzhlm-MBA15 <118811618+rzhlm@users.noreply.github.com>
2024-10-20 21:43:14 +0200

    worked on the storage factory & classes

--- src/storage.py
+++ src/storage.py
@@ -1,7 +1,7 @@
 from constants import StorageType
 
 class StorageStrategy:
-    def add_habit(self, habit):
+    def add_habit(self):
         pass
     def load_all(self):
         pass
@@ -9,19 +9,39 @@ class StorageStrategy:
         pass
 
 class StorageFactory:
-    def create_storage(storage_type, save_file):
-        match storage_type:
-            case StorageType.PICKLE:
-                return PickleStorage(save_file)
-            case StorageType.JSON:
-                return JSONStorage(save_file)
-            case StorageType.SQLITE:
-                return SQLiteStorage(save_file)
-            case _:
-                raise Exception("StorageFactory: no storage type given!")
+    unique = False
+    """     
+    def __call__(cls, storage_type, save_file):
+    if cls not in cls.unique:
+        cls.unique[cls] = super(cls).__call__(storage_type,save_file)
+    return cls.unique[cls]
+    """
+    
+    def create_storage(cls, storage_type, save_file):
+        
+            match storage_type:
+                case StorageType.PICKLE:
+                    st = PickleStorage(save_file)
+                    return st
+                case StorageType.JSON:
+                    st = JSONStorage(save_file)
+                    return st
+                case StorageType.SQLITE:
+                    st = SQLiteStorage(save_file)
+                    return st
+                case _:
+                    raise Exception("StorageFactory: no storage type given!")
+            return cls.unique[cls]
 
 class JSONStorage(StorageStrategy):
-    pass
+    def __init__(self, savefile):
+        self.savefile = savefile
+    
+    def load_all(self):
+        pass
+
+    def save_all(self):
+        pass
     
 class PickleStorage(StorageStrategy):
     def __init__(self, savefile):
@@ -34,4 +54,11 @@ class PickleStorage(StorageStrategy):
         pass
     
 class SQLiteStorage(StorageStrategy):
-    pass
+    def __init__(self, savefile):
+        pass
+
+    def load_all(self):
+        pass
+
+    def save_all(self):
+        pass
